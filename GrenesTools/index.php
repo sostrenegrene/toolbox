4072432f45f7531c2d2c2c91170cd2e453ff1df7
soren.pedersen <soren.pedersen@Laptop026.grenes.local>
2016-02-12 19:33:19 +0100

    Minor update

--- GrenesTools/index.php
+++ GrenesTools/index.php
@@ -4,13 +4,15 @@ session_start();
 require_once 'system/config/config.php';
 
 require_once 'system/sys_lib/getset.php';
-require_once 'system/sys_lib/mysql.php';
+//require_once 'system/sys_lib/mysql.php';
+require_once 'system/sys_lib/mssql.php';
 
 require_once 'system/includes/Menus.class.php';
 require_once 'system/includes/Modules.class.php';
 
 $getset = new GetSet();
-$db 	= new MySQL(DB_HOST, DB_USER, DB_PASS, DB_DB);
+//$db 	= new MySQL(DB_HOST, DB_USER, DB_PASS, DB_DB);
+$db 	= new MSSQL(DB_HOST, DB_USER, DB_PASS, DB_DB);
 
 //Save "load" id to session
 $getset->setSession("load",$getset->header("load"));
--- GrenesTools/system/config/config.php
+++ GrenesTools/system/config/config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","");
-define("DB_DB","sostrenegrene");
+define("DB_HOST","(local)");
+define("DB_USER","Toolbox");
+define("DB_PASS","Toolbox4U");
+define("DB_DB","Toolbox");
 
 require_once 'constants.php';
 ?>
